92ecd7baee525d92e20ecfe5214150c54d2dc258
Ed Hirst <edthirst@gmail.com>
2024-06-10 20:21:42 +1000

    added some thing but i am totally fucked

---
+++ sat/bestRoute.h
@@ -0,0 +1,12 @@
+# pragma once
+
+# include <iostream>
+# include "adts/linkedlist.h"
+# include "adts/graph.h"
+# include "adts/pair.h"
+# include "sat/goal.h"
+# include "sat/team.h"
+
+linkedList<node<goal> > bestRoute(graph<goal> g, const node<goal>* startNode, const node<goal>* endNode, int timeLimit, const float walkSpeed) {
+
+};
