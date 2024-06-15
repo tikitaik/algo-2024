ae014e1f22ae8ffba7c16c3fc9e2da1bb364eeab
Ed Hirst <edthirst@gmail.com>
2024-06-15 13:48:06 +1000

    going to change every goal to checkpoint

--- sat/bestRoute.h
+++ sat/bestRoute.h
@@ -7,6 +7,7 @@
 # include "sat/goal.h"
 # include "sat/team.h"
 
-linkedList<node<goal> > bestRoute(graph<goal> g, const node<goal>* startNode, const node<goal>* endNode, int timeLimit, const float walkSpeed) {
+// given a bunch of inputs return the time estimate for a path between checkpoints
+linkedList<node<goal> > getTimeOfEdge(const node<goal>* startNode, const node<goal>* endNode, const int distInPixels) {
 
 };
